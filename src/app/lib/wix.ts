98b3d684993d4fa481fabbd490ce0dd664052544
shakthishamruth <shakthishamruth@gmail.com>
2025-08-01 15:17:38 +0530

    update stories

--- src/app/lib/wix.ts
+++ src/app/lib/wix.ts
@@ -1,9 +1,10 @@
 import { createClient, OAuthStrategy } from "@wix/sdk";
 import { items } from "@wix/data";
 const client = createClient({
-modules: { items },
-auth: OAuthStrategy({
-    clientId:process.env.WIX_CLIENT_ID!,
-}),
+    modules: { items },
+    auth: OAuthStrategy({
+        clientId: process.env.WIX_CLIENT_ID!,
+    }),
 })
+
 export default client;
--- src/app/stories/page.tsx
+++ src/app/stories/page.tsx
@@ -1,7 +1,22 @@
-export default function Home() {
+import client from "../lib/wix";
+
+export default async function Home() {
+
+    const stories = await client.items.queryDataItems({
+        dataCollectionId: "Stories"
+    })
+        .find()
+        .then((res) => res.items.map((item) => item.data));
+
+    console.log(stories);
+
     return (
         <div className="grid grid-rows-[20px_1fr_20px] items-center">
             <h1 className="text-2xl font-bold">Stories</h1>
+
+            <div className="grid grid-col-3 gap-4">
+
+            </div>
         </div>
     );
 }
