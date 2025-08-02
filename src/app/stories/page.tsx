deb0a18b8cc009f1224a574acd2698f257ce4a9a
shakthishamruth <shakthishamruth@gmail.com>
2025-08-02 10:44:34 +0530

    update stories

--- src/app/stories/page.tsx
+++ src/app/stories/page.tsx
@@ -2,21 +2,28 @@ import client from "../lib/wix";
 
 export default async function Home() {
 
-    const stories = await client.items.queryDataItems({
-        dataCollectionId: "Stories"
-    })
-        .find()
-        .then((res) => res.items.map((item) => item.data));
+    if (!process.env.WIX_CLIENT_ID) {
+        throw new Error("WIX_CLIENT_ID is not defined in your environment.");
+    }
 
+
+    const result = await client.items.query("Stories").find();
+    const stories = result.items;
     console.log(stories);
 
     return (
         <div className="grid grid-rows-[20px_1fr_20px] items-center">
             <h1 className="text-2xl font-bold">Stories</h1>
 
-            <div className="grid grid-col-3 gap-4">
-
+            <div className="grid grid-cols-3 gap-4">
+                {stories.map((story, idx) => (
+                    <div key={story.id ?? idx}>
+                        <h2>{story.title}</h2>
+                        <p>{story.author}</p>
+                    </div>
+                ))}
             </div>
+
         </div>
     );
 }
